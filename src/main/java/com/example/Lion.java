13e6fa65e3a23abafa78471110f8d39500265d8f
ZhannaDan <zh.i.danyaeva@gmail.com>
2025-07-27 21:24:52 +0300

    sprint5 zhanna danyaeva

--- src/main/java/com/example/Lion.java
+++ src/main/java/com/example/Lion.java
@@ -15,18 +15,4 @@ public class Lion {
             throw new Exception("Используйте допустимые значения пола животного - самей или самка");
         }
     }
-
-    Feline feline = new Feline();
-
-    public int getKittens() {
-        return feline.getKittens();
-    }
-
-    public boolean doesHaveMane() {
-        return hasMane;
-    }
-
-    public List<String> getFood() throws Exception {
-        return feline.getFood("Хищник");
-    }
 }
